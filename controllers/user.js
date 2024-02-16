1916f165545eed9b2f4158d6149ae23f49c11897
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2024-02-16 12:30:50 +0530

    login validation

--- controllers/user.js
+++ controllers/user.js
@@ -15,7 +15,7 @@ exports.postUser = async (req, res, next) => {
             password: password
         });
 
-        console.log(user.toJSON());
+        console.log(user);
         // userJS = user.toJSON();
 
         res.json({createdUser: user});
@@ -26,4 +26,27 @@ exports.postUser = async (req, res, next) => {
         res.status(500).json(err);
     }
 
-}
+};
+
+exports.postLogin = async (req, res, next) => {
+    try {
+        let email = req.body.email;
+        let password = req.body.password;
+
+        const user = await User.findAll({
+            where: {
+                email: email
+            }
+        });
+
+        console.log(user.length);
+
+        if (user.length) {
+            res.status(200).json(user);
+        } else{
+            res.status(404).json(user);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,6 @@ const router = express.Router();
 
 router.post('/signup', userController.postUser);
 
+router.post('/login', userController.postLogin);
+
 module.exports = router;
