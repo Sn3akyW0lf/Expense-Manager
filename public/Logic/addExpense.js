1784cbdded399483955088ece559bacb44d50e9a
Sn3akyW0lf <144889693+Sn3akyW0lf@users.noreply.github.com>
2024-03-06 12:24:58 +0530

    added actual front end files to the repo

---
+++ public/Logic/addExpense.js
@@ -0,0 +1,56 @@
+const myForm = document.querySelector('#my-form');
+const expense = document.querySelector('#expense');
+const exp_desc = document.querySelector('#exp_desc');
+const exp_type = document.querySelector('#exp_type');
+const msg_exp = document.querySelector('#msg_exp');
+const msg_exp_type = document.querySelector('#msg_exp_type');
+const msg_desc = document.querySelector('#msg_desc');
+
+myForm.addEventListener('submit', onSubmit);
+
+
+async function onSubmit(e) {
+    try {
+        e.preventDefault();
+
+        const token = localStorage.getItem('token');        
+
+        if (expense.value === '') {
+            msg_exp.style.color = 'chocolate';
+            msg_exp.style.background = 'beige';
+            msg_exp.innerHTML = 'Please Enter Expense Amount!';
+            setTimeout(() => msg_exp.remove(), 3000);
+        } else if (exp_desc.value === '') {
+            msg_desc.style.color = 'chocolate';
+            msg_desc.style.background = 'beige';
+            msg_desc.innerHTML = 'Please Enter Something about Your Expense!';
+            setTimeout(() => msg_desc.remove(), 3000);
+        } else if (exp_type.value === 'none') {
+            msg_exp_type.style.color = 'chocolate';
+            msg_exp_type.style.background = 'beige';
+            msg_exp_type.innerHTML = 'Please Select Expense Type!';
+            setTimeout(() => msg_exp_type.remove(), 3000);
+        } else {
+            let expObj = {
+                amount: expense.value,
+                category: exp_type.value,
+                description: exp_desc.value
+                
+            }
+            console.log(expObj);
+
+
+            let res = await axios.post('http://13.233.152.205:4000/expense/add-expense', expObj, { headers: { 'Authorization' : token } });
+
+            // let res = await axios.post('/expense/add-expense', expObj, { headers: { 'Authorization' : token } });
+            
+
+            window.location.replace('index.html');
+
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+
+}
---
+++ public/Logic/leaderboard.js
@@ -0,0 +1,54 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const token = localStorage.getItem('token');
+    const isPremium = localStorage.getItem('isPremium');
+
+
+    if (isPremium == true) {
+        razorPremium.className = 'd-none';
+
+        let result = await axios.get('http://13.233.152.205:4000/premium/show-leaderboard', {
+        // let result = await axios.get('/premium/show-leaderboard', {
+            headers: {
+                'Authorization': token
+            }
+        });
+
+        // console.log(result);
+
+        premium_leader.className = 'container';
+
+        const data = result.data;
+
+        data.forEach(d => {
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+
+            td1.appendChild(document.createTextNode(`${d.name}`));
+            if (d.totalExpense) {
+                td2.appendChild(document.createTextNode(`${d.totalExpense}`));
+            } else {
+                td2.appendChild(document.createTextNode(0));
+            }
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tblLeader.appendChild(tr);
+
+        });
+    } else {
+        msg_premium.style.color = 'chocolate';
+        msg_premium.className = 'align-self-center';        
+        msg_premium.innerHTML = 'This is a Premium Member Feature, Please Purchase Membership to Access this Page!';
+    }
+
+
+
+})
+
+function premiumMessage() {
+    razorPremium.className = 'd-none';
+    msg_premium.style.color = 'chocolate';
+    msg_premium.innerHTML = 'You Are a Premium Member!';
+    btnLeader.className = 'btn btn-warning btn-sm btn-outline-dark p-3 m-3';
+}
